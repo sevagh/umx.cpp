4d2cc1639f39a27665f65c687453c35fe81e9790
Sevag Hanssian <sevagh@protonmail.com>
2023-07-27 23:56:43 -0400

    Remove unused tensors

--- src/tensor.hpp
+++ src/tensor.hpp
@@ -11,12 +11,6 @@ namespace Eigen
 // define Tensor3dXf, Tensor3dXcf for spectrograms etc.
 typedef Tensor<float, 3> Tensor3dXf;
 typedef Tensor<std::complex<float>, 3> Tensor3dXcf;
-
-typedef Tensor<float, 4> Tensor4dXf;
-typedef Tensor<std::complex<float>, 4> Tensor4dXcf;
-
-typedef Tensor<float, 1> Tensor1dXf;
-// typedef Tensor<std::complex<float>, 4> Tensor4dXcf;
 } // namespace Eigen
 
 #endif // TENSOR_HPP
